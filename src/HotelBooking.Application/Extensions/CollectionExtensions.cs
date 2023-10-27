c9a6f3c587e1af628f87e77c409548c17feb8375
mmm3031 <138799337+mmm3030@users.noreply.github.com>
2023-10-28 04:16:13 +0700

    add class helpers

---
+++ src/HotelBooking.Application/Extensions/CollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace HotelBooking.Application.Extensions
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class CollectionExtensions
+    {
+
+        public static T Find<T>(this T[] items, Predicate<T> predicate)
+        {
+            return Array.Find(items, predicate);
+        }
+
+        public static T[] FindAll<T>(this T[] items, Predicate<T> predicate)
+        {
+            return Array.FindAll(items, predicate);
+        }
+
+        public static bool IsNullOrEmpty(this IEnumerable @this)
+        {
+            if (@this != null)
+            {
+                return !@this.GetEnumerator().MoveNext();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/HotelBooking.Application/Helpers/PaginatedResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace HotelBooking.Application.Helpers;
+public class PaginatedResponse<T> where T : class
+{
+    public int Page { get; }
+    public int Size { get; }
+    public int TotalPages { get; }
+    public int TotalItems { get; }
+    public bool HasPreviousPage { get; }
+    public bool HasNextPage { get; }
+    public IList<T> Contends { get; }
+
+    public PaginatedResponse(PaginatedList<T> paginatedList)
+    {
+        Page = paginatedList.PageIndex;
+        Size = paginatedList.PageSize;
+        TotalPages = paginatedList.TotalPages;
+        TotalItems = paginatedList.TotalItems;
+        HasPreviousPage = paginatedList.HasPreviousPage;
+        HasNextPage = paginatedList.HasNextPage;
+        Contends = paginatedList;
+    }
+
+}
+
+public static class PaginatedResponseExtensions
+{
+    public static PaginatedResponse<T> ToPaginatedResponse<T>(this PaginatedList<T> source) where T : class
+    {
+        return new PaginatedResponse<T>(source);
+    }
+}
