735b2ad4cc38ec5871ae049a76e36ebf96240814
mmm3031 <138799337+mmm3030@users.noreply.github.com>
2023-11-03 02:33:50 +0700

    Update Guest entity properties

--- src/HotelBooking.Domain/Entities/Guest.cs
+++ src/HotelBooking.Domain/Entities/Guest.cs
@@ -3,9 +3,11 @@
 namespace HotelBooking.Domain.Entities;
 public class Guest : BaseAuditableEntity
 {
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
-    public string Email { get; set; } = default!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
+    public string? AvatarUrl { get; set; }
+    public string? ProviderKey { get; set; }
     public virtual ICollection<Reservation> Reservations { get; set; } = new HashSet<Reservation>();
 }
