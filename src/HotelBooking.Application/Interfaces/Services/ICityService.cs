91b395ac4f23f853611e45a86139e5b324bb71f6
mmm3031 <138799337+mmm3030@users.noreply.github.com>
2023-11-04 00:25:44 +0700

    Add CityResponse DTO, ICityService interface, and CityService implementation, CitiesController

---
+++ src/HotelBooking.Application/Interfaces/Services/ICityService.cs
@@ -0,0 +1,8 @@
+﻿using HotelBooking.Application.DTOs.Cities;
+using HotelBooking.Application.Helpers;
+
+namespace HotelBooking.Application.Interfaces.Services;
+public interface ICityService
+{
+    Task<PaginatedList<CityResponse>> GetTopDestinationsAsync(int limit);
+}
