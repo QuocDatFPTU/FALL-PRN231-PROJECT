9c64b16e8149779f343abb232aacdf3f3cfe3295
DatQuoc <datquoc3003@gmail.com>
2023-11-06 20:31:16 +0700

    draft: add endpoint payment

---
+++ src/HotelBooking.WebApi/Controllers/PaymentController.cs
@@ -0,0 +1,27 @@
+﻿
+using HotelBooking.Application.DTOs.Users;
+using HotelBooking.Application.Interfaces.Services;
+using HotelBooking.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace HotelBooking.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PaymentController : ControllerBase
+{
+    private readonly IAuthService _authService;
+    public PaymentController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    //[HttpPost("payment")]
+    ////public async Task<ActionResult<UserResponse>> ProcessPayment()
+    ////{
+    ////    string ipAddress;
+    ////    ipAddress = HttpContext.Request. ServerVariables["HTTP_X_FORWARDED_FOR"];
+    ////}
+
+}
