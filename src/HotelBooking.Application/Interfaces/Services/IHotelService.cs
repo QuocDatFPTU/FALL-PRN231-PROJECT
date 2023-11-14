488e429468724eb64289f425a643f5e71e6d825c
mmm3031 <138799337+mmm3030@users.noreply.github.com>
2023-11-14 07:09:19 +0700

    Refactor hotel search functionality with new specifications and sorting options

--- src/HotelBooking.Application/Interfaces/Services/IHotelService.cs
+++ src/HotelBooking.Application/Interfaces/Services/IHotelService.cs
@@ -6,7 +6,7 @@ using HotelBooking.Application.Helpers;
 namespace HotelBooking.Application.Interfaces.Services;
 public interface IHotelService
 {
-    Task<PaginatedResponse<HotelResponse>> GetHotelsAsync(HotelSearchRequest hotelSearchRequest);
+    Task<HotelSearchFilterResponse> GetHotelsAsync(HotelSearchRequest hotelSearchRequest);
     Task<HotelDetailResponse> FindHotelAsync(RoomTypeSearchRequest roomTypeSearchRequest);
     Task<PaginatedResponse<ReviewResponse>> GetReviewsAsync(int hotelId, int pageIndex, int pageSize);
 }
---
+++ src/HotelBooking.Application/Specifications/BaseSpecification.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace HotelBooking.Application.Specifications;
+public class BaseSpecification<T> : ISpecifications<T>
+{
+    public Expression<Func<T, bool>>? Criteria { get; set; }
+    public BaseSpecification(Expression<Func<T, bool>>? Criteria)
+    {
+        this.Criteria = Criteria;
+    }
+
+    public List<Expression<Func<IQueryable<T>, IIncludableQueryable<T, object>>>> Includes { get; } = new();
+
+    public Func<IQueryable<T>, IOrderedQueryable<T>>? OrderBy { get; private set; }
+
+    protected void AddInclude(Expression<Func<IQueryable<T>, IIncludableQueryable<T, object>>> includeExpression)
+    {
+        Includes.Add(includeExpression);
+    }
+
+    public void AddOrderBy(Expression<Func<T, object>> OrderByexpression)
+    {
+        OrderBy = a => a.OrderBy(OrderByexpression);
+    }
+    public void AddOrderByDecending(Expression<Func<T, object>> OrderByDecending)
+    {
+        OrderBy = a => a.OrderByDescending(OrderByDecending);
+    }
+
+}
