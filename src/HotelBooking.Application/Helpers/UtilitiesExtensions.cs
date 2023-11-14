8dfd014d9276c3ddf1e16aaa56e1db6238a1dfcb
mmm3031 <138799337+mmm3030@users.noreply.github.com>
2023-11-14 07:08:21 +0700

    Add async to ToPaginatedResponse method and add expression extension methods.

--- src/HotelBooking.Application/Helpers/UtilitiesExtensions.cs
+++ src/HotelBooking.Application/Helpers/UtilitiesExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Collections;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
@@ -96,5 +97,79 @@ public static class UtilitiesExtensions
             }
         }
         return "127.0.0.1";
+    }
+
+    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+    {
+        Expression<Func<T, bool>> combined = Expression.Lambda<Func<T, bool>>(
+            Expression.And(
+                left.Body,
+                new ExpressionParameterReplacer(right.Parameters, left.Parameters).Visit(right.Body)
+            ), left.Parameters);
+
+        return combined;
+    }
+
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+    {
+        Expression<Func<T, bool>> combined = Expression.Lambda<Func<T, bool>>(
+            Expression.Or(
+                left.Body,
+                new ExpressionParameterReplacer(right.Parameters, left.Parameters).Visit(right.Body)
+            ), left.Parameters);
+
+        return combined;
+    }
+
+    public static Expression<Func<T, bool>> AndAlso<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+    {
+        Expression<Func<T, bool>> combined = Expression.Lambda<Func<T, bool>>(
+            Expression.AndAlso(
+                left.Body,
+                new ExpressionParameterReplacer(right.Parameters, left.Parameters).Visit(right.Body)
+            ), left.Parameters);
+
+        return combined;
+    }
+
+    public static Expression<Func<T, bool>> OrElse<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
+    {
+        Expression<Func<T, bool>> combined = Expression.Lambda<Func<T, bool>>(
+            Expression.OrElse(
+                left.Body,
+                new ExpressionParameterReplacer(right.Parameters, left.Parameters).Visit(right.Body)
+            ), left.Parameters);
+
+        return combined;
+    }
+
+}
+
+public class ExpressionParameterReplacer : ExpressionVisitor
+{
+    private IDictionary<ParameterExpression, ParameterExpression> ParameterReplacements { get; set; }
+
+    public ExpressionParameterReplacer(
+        IList<ParameterExpression> fromParameters,
+        IList<ParameterExpression> toParameters)
+    {
+        ParameterReplacements = new Dictionary<ParameterExpression, ParameterExpression>();
+
+        for (int i = 0; i != fromParameters.Count && i != toParameters.Count; i++)
+        {
+            ParameterReplacements.Add(fromParameters[i], toParameters[i]);
+        }
+    }
+
+    protected override Expression VisitParameter(ParameterExpression node)
+    {
+        ParameterExpression replacement;
+
+        if (ParameterReplacements.TryGetValue(node, out replacement))
+        {
+            node = replacement;
+        }
+
+        return base.VisitParameter(node);
     }
 }
